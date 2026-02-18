7ee798d11442720d1922591f260ff427954be36a
davidmolgard <molgarddavid@gmail.com>
2026-02-17 20:41:40 -0700

    logout started needs mock

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -102,7 +102,16 @@ test('login', async ({ page }) => {
   await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
 });
 
-
+test('logout', async ({ page }) => {
+  await basicInit(page);
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('a');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
+  
+  await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+});
 
 test('purchase with login', async ({ page }) => {
   await basicInit(page);
